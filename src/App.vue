3d1036f4e3f6da60b64d2a35fb7730e7d45274fc
Chance <chance3009@outlook.com>
2024-01-14 17:40:57 +0800

    Complete search function and some styling

--- src/App.vue
+++ src/App.vue
@@ -23,18 +23,65 @@ const handleSignOut = () => {
 
 <template>
   <div>
-    <nav>
-      <router-link to="/"> Home </router-link> |
-      <span v-if="isLoggedIn">
-        <router-link to="/courses-list"> Courses </router-link> |
-        <router-link to="/upload-paper"> Upload Paper </router-link> |
-        <button @click="handleSignOut">Logout</button>
-      </span>
-      <span v-else>
-        <router-link to="/register"> Register </router-link> |
-        <router-link to="/sign-in"> Login </router-link>
-      </span>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/">ECLIPSE RED</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item" :class="{ active: $route.path === '/' }">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link to="/courses-list" class="nav-link"
+              >Courses</router-link
+            >
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link to="/upload-paper" class="nav-link"
+              >Upload Paper</router-link
+            >
+          </li>
+        </ul>
+
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+
+        <span v-if="isLoggedIn" class="ml-md-auto">
+          <button @click="handleSignOut" class="btn btn-outline-light ml-2">
+            Logout
+          </button>
+        </span>
+        <span v-else class="ml-md-auto">
+          <router-link to="/register" class="btn btn-outline-light ml-2"
+            >Register</router-link
+          >
+          <router-link to="/sign-in" class="btn btn-outline-light ml-2"
+            >Login</router-link
+          >
+        </span>
+      </div>
     </nav>
+
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,116 +1,51 @@
 <template>
-  <h1>Home</h1>
-  <div class="search-bar">
-    <input type="text" v-model="searchQuery" placeholder="Search for papers..." @input="performSearch">
-    <button @click="performSearch">Search</button>
-  </div>
-  <div class="search-results">
-    <div v-for="item in filteredItems" :key="item.id" class="search-item">
-      {{ item.title }}
+  <div>
+    <h1>Home</h1>
+    <div class="search-bar">
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search for papers..."
+        @input="performSearch"
+      />
+      <button @click="performSearch">Search</button>
+    </div>
+    <div class="search-results">
+      <PaperCard v-for="paper in papers" :key="paper.id" :paper="paper" />
     </div>
   </div>
-
-
-
-  <h2>Just a home, access more function by register/login</h2>
-  <h3>Till 11/1, I have completed a few function as belows:</h3>
-  <ul>
-    <li>Configure Firebase Authentication</li>
-    <li>Display list of courses</li>
-    <li>Upload new paper (Without the function of adding question)</li>
-    <li>
-      Display list of papers for a specific course after clicking on the
-      respective course button
-    </li>
-    <li>
-      Display list of questions for a specific paper after clicking on the
-      respective paper button
-    </li>
-    <li>Generate new paper (for view)</li>
-    <li>Download the generated paper as txt file</li>
-  </ul>
-  <p>
-    Just to clarify, for now I just throw everything into views and write
-    everything as individual file, but certain files should be classfied as
-    components instead, so who are available please help to organize the project
-    file structure, make it less cluttered lah. Need to refactor.
-  </p>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#"
-            >Home <span class="sr-only">(current)</span></a
-          >
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input
-          class="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
-    </div>
-  </nav>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { coursesRef } from "../firebase/db.js";
-import { getDocs } from "firebase/firestore";
+import { examsRef } from "../firebase/db.js";
+import { query, where, getDocs } from "firebase/firestore";
+import PaperCard from "../components/PaperCard.vue";
 
-const courses = ref([]);
+const searchQuery = ref("");
+const papers = ref([]);
 
-onMounted(async () => {
-  let coursesCollection = await getDocs(coursesRef);
-  coursesCollection.forEach((course) => {
-    console.log(course.data());
-    courses.value.push({ ...course.data(), id: course.id });
-  });
+const performSearch = async () => {
+  const query = searchQuery.value.toUpperCase();
+
+  try {
+    const q = query(
+      examsRef,
+      where("__name__", ">=", `${query}`),
+      where("__name__", "<", `${query}\uf8ff`)
+    );
+    const snapshot = await getDocs(q);
+    papers.value = snapshot.docs.map((paper) => ({
+      id: paper.id,
+      ...paper.data(),
+    }));
+  } catch (error) {
+    console.error("Error performing search:", error);
+  }
+};
+
+onMounted(() => {
+  // You can perform an initial search on component mount if needed
+  // performSearch();
 });
 </script>
-
-
