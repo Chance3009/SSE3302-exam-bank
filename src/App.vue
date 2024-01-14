8d81514e3e21245d0f113577c441850c680ed3da
Chai Kok Cheng <94617307+chaikokcheng@users.noreply.github.com>
2024-01-14 12:54:00 +0800

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -23,18 +23,51 @@ const handleSignOut = () => {
 
 <template>
   <div>
-    <nav>
-      <router-link to="/"> Home </router-link> |
+    <nav class="navbar">
+      <router-link class="nav-link" to="/">
+        <img src="./assets/SEExam.png" alt="Logo" style="height: 30px; vertical-align: middle; margin-right: 5px;" />
+        <span>SE EXAM BANK</span> </router-link> |
       <span v-if="isLoggedIn">
-        <router-link to="/courses-list"> Courses </router-link> |
-        <router-link to="/upload-paper"> Upload Paper </router-link> |
-        <button @click="handleSignOut">Logout</button>
+        <router-link class="nav-btn" to="/courses-list">Courses</router-link> |
+        <router-link class="nav-btn" to="/upload-paper">Upload Paper</router-link> |
+        <button class="nav-btn" @click="handleSignOut">Logout</button>
       </span>
       <span v-else>
-        <router-link to="/register"> Register </router-link> |
-        <router-link to="/sign-in"> Login </router-link>
+        <router-link class="nav-btn" to="/register">Register</router-link> |
+        <router-link class="nav-btn" to="/sign-in">Login</router-link>
       </span>
     </nav>
     <router-view />
   </div>
 </template>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-around;
+  background-color: #f5f5f5;
+}
+
+.nav-btn,
+.nav-link {
+  padding: 10px 20px;
+  margin: 5px;
+  border: none;
+  border-radius: 10px;
+  background-color: white;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.nav-btn:hover {
+  background-color: #e0e0e0;
+
+}
+
+.nav-link {
+  background-color: transparent;
+  border: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,32 +9,6 @@
       {{ item.title }}
     </div>
   </div>
-
-
-
-  <h2>Just a home, access more function by register/login</h2>
-  <h3>Till 11/1, I have completed a few function as belows:</h3>
-  <ul>
-    <li>Configure Firebase Authentication</li>
-    <li>Display list of courses</li>
-    <li>Upload new paper (Without the function of adding question)</li>
-    <li>
-      Display list of papers for a specific course after clicking on the
-      respective course button
-    </li>
-    <li>
-      Display list of questions for a specific paper after clicking on the
-      respective paper button
-    </li>
-    <li>Generate new paper (for view)</li>
-    <li>Download the generated paper as txt file</li>
-  </ul>
-  <p>
-    Just to clarify, for now I just throw everything into views and write
-    everything as individual file, but certain files should be classfied as
-    components instead, so who are available please help to organize the project
-    file structure, make it less cluttered lah. Need to refactor.
-  </p>
 </template>
 
 <script setup>
